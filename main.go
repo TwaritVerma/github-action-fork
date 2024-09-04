fc35b8799148264c077f2ca4dd4a080e5c84a261
AntonyRohithAkash <93250352+AntonyRohithAkash@users.noreply.github.com>
2024-09-04 13:55:44 +0530

    secret

--- main.go
+++ main.go
@@ -2,8 +2,15 @@ package main
 
 import (
     "fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println("TEST")
+	secret := os.Getenv("SECRET_KEY")
+	fmt.Println(secret)
+	if secret != "" {
+		fmt.Println("WRONG SECRET")
+		os.Exit(1)
+	}
+	fmt.Println("Completed")
 }
